7ae5c61c4a6970cf1564f55dd2855b0fd195ce31
ngovuhuy <ngovuhuy2016@gmail.com>
2024-12-05 23:48:13 +0700

    Completed read by expenseId

--- src/services/expense-service.ts
+++ src/services/expense-service.ts
@@ -3,4 +3,9 @@ import { Expense } from "../model/Expense"
 
 export const getExpenses = () => {
    return apiClient.get<Expense[]>('/expenses');
+
+}
+
+export const getExpenseByExpenseId = (expenseId: string) => {
+   return  apiClient.get<Expense>(`/expenses/${expenseId}`);
 }
