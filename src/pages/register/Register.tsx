1cf078ba5669376203964668a071dd8f1de5c30b
ngovuhuy <ngovuhuy2016@gmail.com>
2024-12-17 23:09:49 +0700

    completed register

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
-
+import profileValidationSchema from '../../validation/profileValidationSchema'
+import {Profile} from "../../model/Profile";
+import { useFormik } from 'formik';
+import { useRegister } from '../../hooks/useRegister';
 const Register = () => {
+   const{error,isLoading,signup,toast} = useRegister();
+  const formik = useFormik<Profile>({
+  initialValues:{
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  },
+  validationSchema: profileValidationSchema,
+  onSubmit: (profile: Profile, {resetForm}) => {
+    signup(profile);
+    resetForm();
+  },
+  });
   return (
-    <div>Register</div>
+      <div className="d-flex justify-content-center align-items-center login-background">
+         <div className="container col-md-4 col-sm-12">
+          {isLoading && <p className='text-danger'>Loading...</p>}
+          {error && <p className='text-danger'>{error}</p>}
+          {toast && <p className='text-success'>{toast}</p>}
+           <form onSubmit={formik.handleSubmit}>
+           <div className="mb-3">
+           <label htmlFor="name" className='form-label'>Name</label>
+            <input type="text"  className='form-control' id='name' placeholder='Enter your name' name='name' value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+          {formik.touched.name && formik.errors.name ?   <div className="text-danger fst-italic">{formik.errors.name}</div> : null}
+           </div>
+
+           <div className="mb-3">
+           <label htmlFor="email" className='form-label'>Email</label>
+            <input type="email"  className='form-control' id='email' placeholder='Enter your email' name='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            {formik.touched.email && formik.errors.email ? <div className="text-danger fst-italic">{formik.errors.email}</div> : null}
+           </div>
+
+           <div className="mb-3">
+           <label htmlFor="password" className='form-label'>Password</label>
+            <input type="password"  className='form-control' id='password' placeholder='Enter your password' name='password' value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+           {formik.touched.password && formik.errors.password ?  <div className="text-danger fst-italic">{formik.errors.password }</div> : null}
+           </div>
+
+           <div className="mb-3">
+           <label htmlFor="retypePassword" className='form-label'>Retype Password</label>
+            <input type="password"  className='form-control' id='retypePassword' placeholder='Enter your confirm password' name='confirmPassword' value={formik.values.confirmPassword} onChange={formik.handleChange} onBlur={formik.handleBlur}/>
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ?  <div className="text-danger fst-italic">{formik.errors.confirmPassword}</div> : null}
+           </div>
+           <button className='btn btn-sm btn-primary btn-outline-light' type='submit'>Register</button>
+           </form>
+         </div>
+      </div>
   )
 }
 
---
+++ src/validation/profileValidationSchema.ts
@@ -0,0 +1,10 @@
+import * as yup from "yup";
+
+const profileValidationSchema = yup.object().shape({
+    name: yup.string().required('Name is required').min(3, "Name must be at least 3 characters"),
+    email: yup.string().required('Email is required').email('Email is not valid'),
+    password: yup.string().required('Password is required').min(5, 'Password must be at least 5 characters'),
+    confirmPassword: yup.string().required('Retype confirm password').oneOf([yup.ref('password')], 'Password must match')
+});
+
+export default profileValidationSchema;
